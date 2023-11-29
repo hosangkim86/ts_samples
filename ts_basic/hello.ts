8317fd279c61c65748a5d7180f4cc9533115ea99
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-29 11:04:05 +0900

    update

--- ts_basic/hello.ts
+++ ts_basic/hello.ts
@@ -1,8 +1,25 @@
 var message:string = "Hello!!!!";
 console.log(message);
 
+// var id;
+// var email;
+// var isActive;
+// var none;
+
 // 型推論(Type Inference)
 var id = 1;
 var email = "test@test.com";
 var isActive = true;
 var none;
+
+// id = "YSE";
+// email = 1000;
+// isActive = "OK";
+// none = 1000;
+// none = "YSE";
+
+// 型定義(Type definition)
+// var id:number = 1;
+// var email:string = "test@test.com";
+// var isActive:boolean = true;
+// var none:any;
