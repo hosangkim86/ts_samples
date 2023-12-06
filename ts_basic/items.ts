2117a9d65046059c9cd7f21546889f1e155787cc
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-06 10:29:36 +0900

    add items.ts

---
+++ ts_basic/items.ts
@@ -0,0 +1,28 @@
+// 配列
+var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
+console.log(itemNames);
+
+//オブジェクト配列
+var item1 = {
+    id: 1,
+    name: "コーヒー",
+    price: 350,
+}
+var item2 = {
+    id: 2,
+    name: "紅茶",
+    price: 400,
+}
+var item3 = {
+    id: 3,
+    name: "ほうじ茶",
+    price: 300,
+}
+
+// var items:object[] = [item1, item2, item3];
+var items = [item1, item2, item3];
+console.log(items);
+
+var borderPrice = 350;
+var filterItems = items.filter((item) => item.price <= borderPrice);
+console.log(filterItems);
--- ts_basic/user.ts
+++ ts_basic/user.ts
@@ -22,32 +22,3 @@ var user = {
 }
 // user.name.first = 1;
 console.log(user);
-
-// 配列
-var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
-console.log(itemNames);
-
-//オブジェクト配列
-var item1 = {
-    id: 1,
-    name: "コーヒー",
-    price: 350,
-}
-var item2 = {
-    id: 2,
-    name: "紅茶",
-    price: 400,
-}
-var item3 = {
-    id: 3,
-    name: "ほうじ茶",
-    price: 300,
-}
-
-// var items:object[] = [item1, item2, item3];
-var items = [item1, item2, item3];
-console.log(items);
-
-var borderPrice = 300;
-var filterItems = items.filter((item) => item.price <= borderPrice);
-console.log(filterItems);
