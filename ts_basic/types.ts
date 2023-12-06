8290a24d4887c8b3694dd6215e9f9fb361a46ab7
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-06 11:53:45 +0900

    update

--- ts_basic/types.ts
+++ ts_basic/types.ts
@@ -8,3 +8,19 @@ export type User = {
     email: string;
     isActive: boolean;
 }
+
+export type Item = {
+    id: number;
+    name: string;
+    price: number;
+}
+
+// export interface User {
+//     id: number;
+//     name: {
+//         first: string;
+//         last: string;
+//     },
+//     email: string;
+//     isActive: boolean;
+// }
