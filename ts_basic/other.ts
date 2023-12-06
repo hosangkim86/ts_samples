da2a40a63d90fadf1f1338409eb997ef49de105a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-06 10:45:32 +0900

    update

--- ts_basic/other.ts
+++ ts_basic/other.ts
@@ -4,8 +4,12 @@ unionData = "Tokyo";
 unionData = 100;
 // unionData = true;
 
+console.log(unionData, typeof unionData);
+
 // any型 = JavaScript（動的型付けで決定）
 var data: any;
 data = 10;
 data = "Hello";
 data = true;
+
+console.log(data, typeof data);
