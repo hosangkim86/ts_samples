ee77c0b3f05d4f9029245fe7e1fcf1a8331c469e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-06 11:24:11 +0900

    update

---
+++ ts_basic/callback.ts
@@ -0,0 +1,18 @@
+function handleRandom(count: number) {
+    const randomNumber = Math.random();
+    const message = `${count}: ${randomNumber}`;
+    console.log(message);
+}
+
+// main
+function start(callback) {
+    var count = 0;
+    const timer = setInterval(()=> {
+        count++;
+        if (count >= 5) clearInterval(timer);
+        callback(count);
+    }, 1000);
+}
+
+//Unit Test
+// handleRandom(1);
